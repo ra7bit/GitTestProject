aae5a148681614ba6d41d1d82a0b415e9add211e
ra7bit <ra7bit@gmail.com>
2013-08-06 13:51:35 +0800

    Update ARSSwitchTest.java

--- src/com/omniture/uiautomation/report/testcases/ARSSwitchTest.java
+++ src/com/omniture/uiautomation/report/testcases/ARSSwitchTest.java
@@ -7,15 +7,15 @@ import com.omniture.uiautomation.report.ui.ReportUI;
 public class ARSSwitchTest extends Report {
 	public ARSSwitchTest() {
 		this.reportToolbarPage.setWebDriver(this.webDriver);
-		try {
+	}
+    public void testRSSwitch() {
+        this.reportToolbarPage.rsSwitch("asc-test1", ReportUI.RS_ASC_TEST1);
+        System.out.println("e1");
+        try {
 			Thread.sleep(18000000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-    public void testRSSwitch() {
-        this.reportToolbarPage.rsSwitch("asc-test1", ReportUI.RS_ASC_TEST1);
-        System.out.println("e1");
     }
 }
